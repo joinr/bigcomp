f846cf2fd896600aa152fa73cf7b9da0e4b80022
tom <tom@that-place>
2024-08-13 15:28:13 -0500

    Added bypass for compile-files

--- src/bigcomp/core.clj
+++ src/bigcomp/core.clj
@@ -4,3 +4,18 @@
 
 (def path (io/resource "config.edn"))
 (def m (-> path slurp edn/read-string))
+
+;;heh, this is a janky way to get it working too...
+(println *compile-files*)
+(if *compile-files*
+  (binding [*compile-files* false]
+    (def skippedm (binding [*ns* *ns*]
+                    (require 'bigcomp.skipped)
+                    (-> (resolve 'bigcomp.skipped/m)
+                        deref))))
+  (def skippedm (do (require 'bigcomp.skipped)
+                    (-> (resolve 'bigcomp.skipped/m)
+                        deref))))
+
+(println [(= m skippedm)])
+
